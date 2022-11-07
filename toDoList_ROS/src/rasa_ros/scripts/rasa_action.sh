b398cd46b91c08b2b739c07992c025d2533a3d21
sudousernameavailable <94124983+sudousernameavailable@users.noreply.github.com>
2022-11-07 07:29:46 -0800

    Delete toDoList_ROS directory

--- toDoList_ROS/src/rasa_ros/scripts/rasa_action.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BOT_DIR="$HOME/Desktop/Cognitive-Robotics/cogrob_chatbot"
-
-cd $BOT_DIR
-
-rasa run actions
--- toDoList_ROS/src/rasa_ros/scripts/rasa_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BOT_DIR="$HOME/Desktop/Cognitive-Robotics/cogrob_chatbot"
-
-cd $BOT_DIR
-
-rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml --enable-api
