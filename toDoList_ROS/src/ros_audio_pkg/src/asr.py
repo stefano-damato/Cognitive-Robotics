5bdf9b5be3a3b06f764a8f2aa4a73af83923c3e2
mettino99 <benedetto99@live.com>
2022-12-11 18:09:19 +0100

    Reidentification

--- toDoList_ROS/src/ros_audio_pkg/src/asr.py
+++ toDoList_ROS/src/ros_audio_pkg/src/asr.py
@@ -11,25 +11,25 @@ r = sr.Recognizer()
 
 # Init node
 rospy.init_node('speech_recognition', anonymous=True)
-pub1 = rospy.Publisher('voice_data', Int16MultiArray, queue_size=10)
-pub2 = rospy.Publisher('voice_txt', String, queue_size=10)
-
+#pub1 = rospy.Publisher('voice_data', Int16MultiArray, queue_size=10)
+#pub2 = rospy.Publisher('voice_txt', String, queue_size=10)
+pub = rospy.Publisher('text_to_bot', String, queue_size=10)
+label = ""
 # this is called from the background thread
 def callback(audio):
-    print(1)
     data = np.array(audio.data,dtype=np.int16)
-    print(2)
     audio_data = AudioData(data.tobytes(), 16000, 2)
-    data = rospy.wait_for_message("ID",String)
-    label = data.data
-    print(3)
+    id = rospy.wait_for_message("ID",String)
     try:
-        print(4)
         spoken_text= r.recognize_google(audio_data, language='it-IT')
-        print(5)
         print("Google Speech Recognition pensa che "+label+" abbia detto: " + spoken_text)
-        pub1.publish(audio) # Publish audio only if it contains words
-        pub2.publish(spoken_text)
+        #pub1.publish(audio) # Publish audio only if it contains words
+        if id.data != label:
+            label = id.data
+            pub.publish("Hi, I am "+ label)
+        else: 
+            pub.publish(spoken_text)
+        
     except sr.UnknownValueError:
         print("Google Speech Recognition non riesce a capire da questo file audio")
     except sr.RequestError as e:
