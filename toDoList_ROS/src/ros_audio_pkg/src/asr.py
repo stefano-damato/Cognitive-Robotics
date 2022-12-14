adf514ec539dcd932ca268a965970cc1966d84bd
mettino99 <benedetto99@live.com>
2022-12-14 09:14:39 +0100

    pepper

--- toDoList_ROS/src/ros_audio_pkg/src/asr.py
+++ toDoList_ROS/src/ros_audio_pkg/src/asr.py
@@ -15,54 +15,48 @@ rospy.init_node('speech_recognition', anonymous=True)
 #pub2 = rospy.Publisher('voice_txt', String, queue_size=10)
 pub = rospy.Publisher('text_to_bot', String, queue_size=10)
 pub2 = rospy.Publisher('text_for_reidentification', String, queue_size=10)
-global label
-label = ""
+
+
+current_user = ""
+def callback1(msg):
+    global id
+    if msg.data!="NO":
+        id=msg.data
+    print("label "+msg.data)
 # this is called from the background thread
 def callback(audio):
-    print("callback")
-    global label
+    global current_user,id
     data = np.array(audio.data,dtype=np.int16)
     audio_data = AudioData(data.tobytes(), 16000, 2)
-    id = rospy.wait_for_message("ID",String)
+    print("id "+id)
     try:
-        spoken_text= r.recognize_google(audio_data, language='it-IT')
-        print("Google Speech Recognition pensa che "+label+" abbia detto: " + spoken_text)
-        #pub1.publish(audio) # Publish audio only if it contains words
-        if id.data != label:
-            label = id.data
-            pub.publish("Hi, I am "+ label)
-        else: 
-            pub.publish(spoken_text)
+        
+        spoken_text= r.recognize_google(audio_data, language='en-GB')
+        print("Google Speech Recognition pensa che "+id+" abbia detto: " + spoken_text)
+        if id!="unknown":
+            if id != current_user:
+                current_user = id
+                print("Hi, I am "+ current_user)
+                pub.publish("Hi, I am "+ current_user)
+            else: 
+                print("text_to_bot"+spoken_text)
+                pub.publish(spoken_text)
+        else:
+            print("text_for_reidentification "+ spoken_text)
+            pub2.publish(spoken_text)
         
     except sr.UnknownValueError:
         print("Google Speech Recognition non riesce a capire da questo file audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-        
-def callback_label(audio):
-    print("callback_label")
-    data = np.array(audio.data,dtype=np.int16)
-    audio_data = AudioData(data.tobytes(), 16000, 2)
-    id = rospy.wait_for_message("ID",String)
-    try:
-        spoken_text= r.recognize_google(audio_data, language='it-IT')
-        print("Google Speech Recognition pensa che "+label+" abbia detto: " + spoken_text)
-        #pub1.publish(audio) # Publish audio only if it contains words
-        pub2.publish(spoken_text)
-        
-    except sr.UnknownValueError:
-        print("Google Speech Recognition non riesce a capire da questo file audio")
+        pub2.publish("RIPETI")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 def listener():
-    print("Devo entrare in callback")
     rospy.Subscriber("mic_data", Int16MultiArray, callback)
-    print("Sono entrato in callback")
-    rospy.Subscriber("mic_data_label", Int16MultiArray, callback_label)
-    print("Sono entrato in callback_label")
+    rospy.Subscriber("ID",String,callback1)
+    #rospy.Subscriber("mic_data_label", Int16MultiArray, callback_label)
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
+
--- toDoList_ROS/src/ros_audio_pkg/src/voice_detection.py
+++ toDoList_ROS/src/ros_audio_pkg/src/voice_detection.py
@@ -5,30 +5,45 @@ import numpy as np
 
 import time
 import speech_recognition as sr
+class WaitTimeoutError(Exception): pass
 
-pub = rospy.Publisher('mic_data', Int16MultiArray, queue_size=10)
-pub2 = rospy.Publisher('mic_data_label', Int16MultiArray, queue_size=10)
-rospy.init_node('voice_detection_node', anonymous=True)
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    data = rospy.wait_for_message("ID",String)
-    label = data.data
-    print(label)
+
+
+def callback(msg):
+    label=msg.data
+    print("voice "+ label)
     if label!="NO":
         print("Recording...")
-        data = np.frombuffer(audio.get_raw_data(), dtype=np.int16)
-        data_to_send = Int16MultiArray()
-        data_to_send.data = data
-        if label=="name_request":
-            pub2.publish(data_to_send)
-        else:
-            pub.publish(data_to_send)
+        with m as source:
+            try:  
+                # listen for 1 second, then check again if the stop function has been called
+                print("here")
+                audio = r.listen(source, timeout=1, phrase_time_limit=5)
+            except WaitTimeoutError:
+                pass
+            else:
+                print("CIAO MONDOOOO")
+                data = np.frombuffer(audio.get_raw_data(), dtype=np.int16)
+                data_to_send = Int16MultiArray()
+                data_to_send.data = data
+                pub.publish(data_to_send)
+                pub2.publish(label)
+        
+
+    
+pub = rospy.Publisher('mic_data', Int16MultiArray, queue_size=10)
+pub2 = rospy.Publisher('ID_label', String, queue_size=10)
+rospy.init_node('voice_detection_node', anonymous=True)
+
+
+
 
 # Initialize a Recognizer
 r = sr.Recognizer()
 
 # Audio source
+global m
 m = sr.Microphone(device_index=None,
                     sample_rate=16000,
                     chunk_size=1024)
@@ -40,9 +55,6 @@ with m as source:
     r.adjust_for_ambient_noise(source,duration=3)  
 print("Calibration finished")
 
-# start listening in the background
-# `stop_listening` is now a function that, when called, stops background listening
-
-stop_listening = r.listen_in_background(m, callback)
+rospy.Subscriber("ID",String,callback)
 
 rospy.spin()
