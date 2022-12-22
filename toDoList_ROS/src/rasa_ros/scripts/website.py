b57e995d92e926edba50d685c648d3344df84076
stefano-damato <s.damato.1999@gmai.com>
2022-12-22 18:23:25 +0100

    website

--- toDoList_ROS/src/rasa_ros/scripts/website.py
+++ toDoList_ROS/src/rasa_ros/scripts/website.py
@@ -9,6 +9,9 @@ from PIL import Image
 import ast
 import os
 import json
+import threading
+from datetime import datetime as dt
+import time
 
 
 def importDict(filepath):
@@ -17,54 +20,101 @@ def importDict(filepath):
     convertedDict=ast.literal_eval(str)
     return convertedDict
 
-def callback(msg):
+def callback():
 
-    name = msg
-    print(os.listdir())
-    if name != "unknown":
-        file_path = name + FILENAME
-        toDoList=importDict(file_path)
-        if(os.path.exists(file_path)):
+    placeholder = st.empty() 
+    prev_toDoList = dict
+    while not rospy.is_shutdown():
+        name = rospy.wait_for_message("ID",String)
+        name = name.data
+        print("received name:" + name)
+        global previous_name
+        dt_string = dt.now().strftime("%m/%d/%Y, %H:%M")
+        if name != "unknown":
+            previous_name = name
+            file_path = "cogrob_chatbot/" + name + FILENAME
             toDoList=importDict(file_path)
-        else:
-            print("Wrong file path!")
-            toDoList={}
 
-        st.title(name + "'s to do list")
-        #image = Image.open('data/diabetes_image.jpg')
-        #st.image(image, use_column_width=True)
-        for key, value in toDoList.items():
-            st.markdown("- " + key + ", " + value[0])
-        st.markdown('''
-        <style>
-        [data-testid="stMarkdownContainer"] ul{
-            padding-left:40px;
-        }
-        </style>
-        ''', unsafe_allow_html=True)
-        if pepper:
-            load_url_node("http://192.168.1.152:8501")
+            if(os.path.exists(file_path)):
+                toDoList=importDict(file_path)
+            else:
+                print("Wrong file path!")
+                toDoList={}
 
-def main():
-    global FILENAME
-    FILENAME="_toDoList.txt"
+            if prev_toDoList != toDoList:
+                
+                prev_toDoList = toDoList
+                toDoList = dict(sorted(toDoList.items(), key=lambda item: item[1][1], reverse=True))
+                
+                placeholder.empty()
+                with placeholder.container():
+                    st.title(name + "'s to do list:")
+                    #image = Image.open('data/diabetes_image.jpg')
+                    #st.image(image, use_column_width=True)
 
-    rospy.init_node('website_node')
-    rospy.Subscriber("ID",String,callback)
+                    i=0
+                    for key, value in toDoList.items():
+                        if value[1] < dt_string:
+                            st.markdown('<li id=li_' + str(i) + ' style="margin-bottom:-8px;" type="disc" class="activity">' + key +  '</li> <span style="margin-bottom:-8px;" class="category">' + value[0] + '<br><span style="margin-bottom:-12px;" class="deadline_expired"> ' + value[1] + '<hr>' , unsafe_allow_html=True)
+                        else:
+                            st.markdown('<li id=li_' + str(i) + ' style="margin-bottom:-8px;" type="circle" class="activity">' + key +  '</li> <span style="margin-bottom:-8px;" class="category">' + value[0], unsafe_allow_html=True)
+                            if value[1] != "12/31/2050, 23:59":
+                                st.markdown('<br><span style="margin-bottom:-12px;" class="deadline"> ' + value[1] + '<hr>' , unsafe_allow_html=True)
+                            else:
+                                st.markdown('<hr>', unsafe_allow_html=True)
+                        i = i + 1
 
-    with open('config.json', 'r') as f:
-        config = json.load(f)
 
-    global pepper
-    pepper = config["PEPPER"]
-    if pepper:
-        rospy.wait_for_service('load_url')
-        load_url_node=rospy.ServiceProxy('load_url', LoadUrl) 
+                st.markdown('''
+                <style>
+                .activity {
+                    font-size:25px !important;
+                }
+                .category {
+                    font-size:20px;
+                    color: grey;
+                    padding-left:50px;
+                }
+                .deadline{
+                    font-size:20px;
+                    color: grey;
+                    padding-left:50px;
+                }
+                .deadline_expired{
+                    font-size:20px;
+                    color: red;
+                    padding-left:50px;
+                }
+                br {
+                    display: block;
+                    margin: 0.5px 0;
+                }
+                hr{
+                    margin-top: 1px;
+                    margin-bottom: 3px;
+                }
 
-    callback("Benedetto")
+                </style>
+                ''', unsafe_allow_html=True)
+                if pepper:
+                    load_url_node("http://192.168.1.152:8501")
+        time.sleep(4)
+    
 
-if __name__ == '__main__':
-    try: 
-        main()
-    except rospy.ROSInterruptException:
-        pass
+global FILENAME, previous_name
+FILENAME="_toDoList.txt"
+previous_name = ""
+
+threading.Thread(target=lambda: rospy.init_node('website_node', disable_signals=True)).start()
+#rospy.Subscriber("ID",String,callback)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+global pepper
+pepper = config["PEPPER"]
+if pepper:
+    rospy.wait_for_service('load_url')
+    load_url_node = rospy.ServiceProxy('load_url', LoadUrl) 
+
+callback()
